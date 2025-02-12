729465340e026eb31e1ccdcb8ffda61df31dfb19
DHEBA-RETHNA <dhebarethna@gmail.com>
2025-02-12 23:22:33 +0530

    2888-Reshape-Data:-Concatenate.py

--- 2888-Reshape-Data:-Concatenate.py
+++ 2888-Reshape-Data:-Concatenate.py
@@ -2,3 +2,4 @@ import pandas as pd
 
 def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     return pd.concat([df1,df2], ignore_index = True, axis = 0)
+    #return df1._append(df2).reset_index(drop=True)
