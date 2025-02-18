8f3deaffc997a12bce976326afd27717843e5977
DHEBA-RETHNA <dhebarethna@gmail.com>
2025-02-18 23:59:16 +0530

    [448] [Time Beats: 49.09%] [Memory Beats: 17.20%] - LeetPush

---
+++ 448-Find-All-Numbers-Disappeared-in-an-Array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        \\\for i in range(1,len(nums)+1):
+            if i in nums:
+                nums = list(filter(lambda x : x != i, nums))
+            else:
+                nums.append(i)
+        return nums\\\
+        ans = set()
+        for i in range(1,len(nums)+1):
+            if i not in nums:
+                ans.add(i)
+        return list(ans)
+        \\\ans = set(range(1, len(nums)+1))
+        for i in nums:
+            if i in ans:
+                ans.remove(i)
+        return list(ans)\\\
