57aba65ff3cb5117f493ee6e9975f7d557f1668b
DHEBA-RETHNA <dhebarethna@gmail.com>
2025-07-21 23:08:15 +0530

    [1512] [Time Beats: 100.00%] [Memory Beats: 49.88%] - LeetPush

---
+++ Number-of-Good-Pairs.c
@@ -0,0 +1,12 @@
+int numIdenticalPairs(int* nums, int numsSize) {
+    int res = 0;
+    for(int i = 0; i < numsSize; i++)
+    {
+        for(int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] == nums[j])
+                ++res;
+        }
+    }
+    return res;
+}
